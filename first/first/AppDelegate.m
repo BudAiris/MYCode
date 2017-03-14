19dff231e174b55dd2229ea4bb9c794b5a9106de
jinhuadiqigan <1243446245@qq.com>
2017-03-14 13:54:27 +0800

    修改控制器

--- first/first/AppDelegate.m
+++ first/first/AppDelegate.m
@@ -18,7 +18,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = [UIViewController new];
+    UIViewController * vc = [[UIViewController alloc]init];
+    self.window.rootViewController = vc;
     [self.window makeKeyAndVisible];
     return YES;
 }
